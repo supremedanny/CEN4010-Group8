35c90e46ae4f753afb5c978ef97ea9c2feac7613
DanielDDG <99294297+DanielDDG@users.noreply.github.com>
2022-03-13 03:05:07 -0400

    Add files via upload

---
+++ GeekText/books/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from isbn_field import ISBNField
+
+# Create your models here.
+
+# Had to make one of the parts from Feature 4 to begin Feature 1.
+# Allows you to create a book and apply the ISBN, genre, and rating.
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    genre = models.CharField(max_length=50)
+    rating = models.FloatField()
+    sales = models.IntegerField()
+    isbn = ISBNField()
+    on_delete=models.DO_NOTHING,
+    related_name="books"
+
+# This serves to give the labels on the database human-readable names.
+
+    def __str__(self):
+        return self.title
+
+myfantasy_list = Book.objects.filter(genre='Fantasy')
