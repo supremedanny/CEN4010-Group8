236779f26b8465c0030ef2ec55ae7e4f268071c5
DanielDDG <99294297+DanielDDG@users.noreply.github.com>
2022-04-14 21:56:23 -0400

    Add files via upload

--- GeekText/books/models.py
+++ GeekText/books/models.py
@@ -1,29 +1,21 @@
 from django.db import models
-#from django.contrib.auth.models import Rating
 from isbn_field import ISBNField
+import operator
 
 # Create your models here.
 
 # Had to make one of the parts from Feature 4 to begin Feature 1.
 # Allows you to create a book and apply the ISBN, genre, and rating.
 
-
-class Genre(models.Model):
-    name = models.CharField(max_length= 150)
-
-    def __str__(self):
-        return self.name
-
 class Book(models.Model):
     title = models.CharField(max_length=150)
-    genre = models.ManyToManyField(Genre, related_name='books')
-    rating = models.FloatField(max_length=3)
-    #rating = models.ForeignKey(avgrating,on_update = models.CASCADE)
+    genre = models.CharField(null=False, blank=True, max_length=150)
+    rating = models.FloatField(null=True, blank=True)
     sales = models.IntegerField(null=True, blank=True)
-    #ISBN = ISBNField(max_length=13)
-    ISBN = models.CharField(max_length=13)
+    isbn = ISBNField(blank=True)
     on_delete=models.DO_NOTHING,
     related_name="books"
+
 
 # This serves to give the labels on the database human-readable names.
 
--- GeekText/books/urls.py
+++ GeekText/books/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
+from .views import GenreFilter, SalesFilter, RatingsFilter, IDFilter
 
+router = routers.DefaultRouter()
+router.register(r'books', views.BookViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('book/', views.home, name = 'book')
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('search/genre/', GenreFilter.as_view(), name='Genre Filter'),
+    path('search/sales/', SalesFilter.as_view(), name='Top Sellers'),
+    path('search/rating/', RatingsFilter.as_view(), name='Ratings'),
+    path('search/id/', IDFilter.as_view(), name='ID Filter')
 ]
